d9f17cfc6a74eb392a419cfe23887b4b5d5b6c8f
royce <royzelgraphics@gmail.com>
2021-11-17 09:14:09 +0800

    Adding Translator

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ require("./style.scss");
 require("./component-js/landing-page");
 require("./component-js/learning-module");
 require("./component-js/learning-module-2");
-require("./component-js/translator");
+// require("./component-js/translator");
 // require("./component-js/handwriting");
